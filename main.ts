58cffd0b2d9ce608e29707a27c9835c8afe19c5c
biaenanda <114228309+biaenanda@users.noreply.github.com>
2022-09-23 08:00:58 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,1 +1,29 @@
-
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("Vai!")
+    basic.clearScreen()
+    basic.pause(randint(1000, 5000))
+    basic.showIcon(IconNames.House)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showLeds(`
+        . . . # #
+        . . . # #
+        . . # # #
+        . . # # #
+        . . # # #
+        `)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showLeds(`
+        # # # . .
+        # # # . .
+        # # # . .
+        # # . . .
+        # # . . .
+        `)
+})
